acc3ea07005ed22d1a9cc4e9aced6615803aafbe
Malene Trab <malene@trab.dk>
2025-12-06 16:43:11 +0100

    First connector and quick test file

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
 """Test file for pybraendstofpriser module."""
+
+import asyncio
+
+from pybraendstofpriser import Braendstofpriser
+from pybraendstofpriser.const import DIESEL, OCTANE_95
+
+
+async def main():
+    """Main test function."""
+    braendstofpriser = Braendstofpriser()
+    product = OCTANE_95
+
+    companies = await braendstofpriser.list_companies()
+    for company in companies:
+        price = await braendstofpriser.get_price(company, product)
+        print(
+            f"Price for {company} - {companies[company]['products'][OCTANE_95]['name']}:",
+            price,
+        )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
